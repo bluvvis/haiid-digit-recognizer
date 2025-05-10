039cbef2069794c915fb0f7bd7424bbc133e4504
Grigorii <144004392+bluvvis@users.noreply.github.com>
2025-05-11 01:45:37 +0300

    Update model.py

--- recognition/model.py
+++ recognition/model.py
@@ -1,1 +1,14 @@
+from sklearn.datasets import load_digits
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+def load_and_train_model():
+    digits = load_digits()
+    X = digits.images.reshape((len(digits.images), -1))
+    y = digits.target
+
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
