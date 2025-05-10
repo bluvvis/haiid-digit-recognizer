75bb5547ea0d06039882f5114a3ae682c3215266
Grigorii <144004392+bluvvis@users.noreply.github.com>
2025-05-11 01:46:13 +0300

    Update explainability.py

--- recognition/explainability.py
+++ recognition/explainability.py
@@ -1,1 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def explain_prediction(model, processed_input):
+    importance = np.mean([tree.feature_importances_ for tree in model.estimators_], axis=0)
+    img_importance = importance.reshape(8, 8)
+
+    plt.imshow(img_importance, cmap='hot')
+    plt.title("Важность пикселей для модели")
+    plt.colorbar()
+    plt.show()
 
