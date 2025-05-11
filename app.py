ea99574c2f71157ab32a62e820cf9781f2a637b8
IbIbIbIbIbI <sharafutdinov13371@gmail.com>
2025-05-11 22:59:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ st.markdown("""
 with st.sidebar:
     st.header("📖 About the app")
     st.write("Recognize handwritten numbers with AI! Upload images and see the result.")
+    st.write("You can use this picture to check⬇️⬇️⬇️")
     st.image("samples/2_example.png", use_container_width=True)
 
 # ---------- Заголовок ----------
